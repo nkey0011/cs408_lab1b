c57a0b2ed80a803ddcf1f03b84c794368fa32aec
nkey0011 <nkey@stu.jsu.edu>
2022-02-04 03:25:25 -0600

    added text fields and button

--- app/src/main/java/com/example/cs408_lab1b/MainActivity.java
+++ app/src/main/java/com/example/cs408_lab1b/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.cs408_lab1b;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 public class MainActivity extends AppCompatActivity {
+    // declare string array reference as private instance field
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
