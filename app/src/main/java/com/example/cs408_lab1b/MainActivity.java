d6946c185241ef1d11124c0110c640de884e4cd4
nkey0011 <nkey@stu.jsu.edu>
2022-02-09 23:00:18 -0600

    Final Commit

--- app/src/main/java/com/example/cs408_lab1b/MainActivity.java
+++ app/src/main/java/com/example/cs408_lab1b/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.cs408_lab1b;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.cs408_lab1b.databinding.ActivityMainBinding;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    // declare string array reference as private instance field
+
+    private ActivityMainBinding binding;
+
+    Button button = findViewById(R.id.getHelp);
+
+    Resources res = getResources();
+    private String[] plotLines = res.getStringArray(R.array.plotLines);
+    Random r = new Random();
+    int randomLine = r.nextInt(plotLines.length);
+
+
+
 
 
     @Override
@@ -18,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.getHelp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                TextView  t = (TextView)view.getRootView().findViewById(R.id.output);
+                t.setText(randomLine);
+
+            }
+        });
+
+
+
+
+
+
     }
 }
